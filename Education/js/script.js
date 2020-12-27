0eac7557ac7ab65074ac3e93d7e661fcfa5d3ef5
Sandro Mirianashvili <mirianashvili.sandro@gmail.com>
2020-12-27 21:25:56 +0400

    add education pages

---
+++ Education/js/script.js
@@ -0,0 +1,64 @@
+const contentDOM = document.getElementById('content');
+const ulDOM = document.getElementById('ul');
+let SELECTED_TAG = 'ALL';
+
+const get_tuts = (tuts, tag = 'All') => {
+    if (tag === 'ALL') {
+        return tuts;
+    }
+
+    return tuts.filter(x => x.tag === tag);
+}
+
+
+const get_tags = (tuts) => {
+    let tags = new Set();
+    let list = ['ALL']
+    tuts.forEach((item) => {
+        tags.add(item.tag);
+    })
+
+    tags.forEach(v => list.push(v));
+
+    return list;
+}
+
+const onTagClick = (tag) => {
+    SELECTED_TAG
+        = tag;
+    view_tags(ulDOM, tuts_list, SELECTED_TAG
+    );
+    view_links(contentDOM, tuts_list, SELECTED_TAG
+    );
+}
+
+const view_tags = (dom, tuts, select = 'ALL') => {
+    dom.innerHTML = '';
+    get_tags(tuts).forEach((item) => {
+        if (select !== item) {
+            dom.innerHTML += `
+                <li><a onClick="onTagClick('`+ item + `')">` + item + `</a></li>
+            `;
+        } else {
+            dom.innerHTML += `
+                <li class="is-active"><a onClick="onTagClick('`+ item + `')">` + item + `</a></li>
+            `;
+        }
+    })
+}
+
+
+
+const view_links = (dom, tuts, select = 'ALL') => {
+    dom.innerHTML = ''
+    get_tuts(tuts, select).forEach((item) => {
+        console.log(item)
+        dom.innerHTML += `
+             <li><a href="">`+ item.name + `</a></li>
+        `
+    })
+}
+
+
+view_tags(ulDOM, tuts_list, SELECTED_TAG);
+view_links(contentDOM, tuts_list, SELECTED_TAG);
---
+++ Education/js/tuts.js
@@ -0,0 +1,27 @@
+const tuts_list = [
+    {
+        tag: 'Python',
+        name: 'Python Tutorial - Python for Beginners [Full Course]',
+        link: 'https://www.youtube.com/watch?v=_uQrJ0TkZlc'
+    },
+    {
+        tag: 'C++',
+        name: 'C++ Programming All-in-One Tutorial Series (10 HOURS!)',
+        link: 'https://www.youtube.com/watch?v=_bYFu9mBnr4'
+    },
+    {
+        tag: 'React Native',
+        name: 'React Native Crash Course 2020',
+        link: 'https://www.youtube.com/watch?v=Hf4MJH0jDb4'
+    },
+    {
+        tag: 'Python',
+        name: 'Web Programming with Flask - Intro to Computer Science - Harvard\'s CS50 (2018)',
+        link: 'https://www.youtube.com/watch?v=zdgYw-3tzfI'
+    },
+    {
+        tag: 'Angular',
+        name: 'Angular Crash Course',
+        link: 'https://www.youtube.com/watch?v=Fdf5aTYRW0E'
+    },
+]
